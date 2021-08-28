12886afdcff566b36a3825965eed5608977547c8
Kilensio <akilensio@gmail.com>
2021-08-28 19:22:22 +0300

    README.md

---
+++ README.md
@@ -0,0 +1,15 @@
+### Get currency rate app
+
+## Тестовое задание.
+NodeJS, Electron
+
+# Используемые api:
+* http://data.fixer.io/api
+* http://country.io/currency.json
+* https://flagpedia.net/download/api
+
+```
+  npm run start / npm run build
+```  
+
+![currency-rate](/public/screenshots/screenClip.png)
--- src/store/actions.js
+++ src/store/actions.js
@@ -4,22 +4,6 @@ const accessKey = 'a973c432275f79c706d6b4af68318291'
 
 const countryLink = 'http://country.io/currency.json'
 
-// const getRequest = async (request, accessKey) => {
-//   // try {
-//     const response = await axios.get(request, accessKey ? {
-//       params: {
-//         access_key: accessKey
-//       }
-//     } : undefined )
-
-//     return response.data
-
-//   // } catch (error) {
-//     // return error
-//     // throw error
-//   // }
-// }
-
 const getRequest = async (request, accessKey) => {
   const response = await fetch(request + (accessKey ? `?access_key=${accessKey}` : ''))
 
